b6db4c3993afb1d59ac595184c754b397af0e71c
Oleksandr1317 <144147883+Oleksandr1317@users.noreply.github.com>
2023-10-30 14:51:03 +0200

    Add files via upload

---
+++ HomeWork_Linq/ChessPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_Linq
+{
+    public class ChessPlayer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string Country { get; set; }
+        public int Rating { get; set; }
+        public int Games { get; set; }
+        public int Birth_Year { get; set; }
+
+        public override string ToString()
+        {
+            return $"Full name {FirstName + " "  + LastName}, Rating = {Rating}, from {Country}, born in {Birth_Year}";
+        }
+
+        public static ChessPlayer ParseFilexlsx(string lile)
+        {
+            string[] parts = lile.Split(';');
+            return new ChessPlayer()
+            {
+                Id = int.Parse(parts[0]),
+                FirstName = parts[1].Split(' ')[0].Trim(),
+                LastName = parts[1].Split(' ')[1].Trim(),
+                Country = parts[3].Trim(),
+                Title = parts[2].Trim(),
+                Rating = int.Parse(parts[4]),
+                Birth_Year = int.Parse(parts[6])
+            };
+        }
+
+    }
+}
---
+++ HomeWork_Linq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace HomeWork_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var players = File.ReadAllLines(@"Top100ChessPlayers.csv")
+                          .Skip(1)
+                          .Select(x => ChessPlayer.ParseFilexlsx(x))
+                          .Where(player => player.Country == "RUS")
+                          .OrderBy(player => player.Birth_Year)
+                          .ToList();
+
+            foreach (var player in players)
+            {
+                Console.WriteLine(player);
+            }
+            Console.ReadLine();
+        }
+    }
+}
